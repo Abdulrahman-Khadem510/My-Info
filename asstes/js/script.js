b651697eca3362d7bababd0b1c317743400f77c1
Abdulrahman-Khadem510 <abdulrahmankhadem0@gmail.com>
2025-11-17 14:00:10 +0430

    Add files via upload

---
+++ asstes/js/script.js
@@ -0,0 +1,78 @@
+// ================= toggle icon navbar =================
+    let menuIcon = document.querySelector('#menu-icon')
+    let navbar = document.querySelector('.navbar')
+
+    menuIcon.onclick = () =>{
+        menuIcon.classList.toggle('fa-xmark');
+        navbar.classList.toggle('active');
+    };
+// ================= scroll section avtive link=================
+    let section = document.querySelectorAll('section');
+    let navlinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () => {
+        section.forEach(sec =>{
+            let top = window.scrollY;
+            let offset = sec.offsetTop -150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if(top >= offset && top < offset + height){
+                navlinks.forEach(links =>{
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            };
+        });
+// ================= sticky navbar =================
+        let header = document.querySelector('header');
+
+        header.classList.toggle('sticky', window.scrollY > 100);
+  
+// ================= remov toggle icon and navbar when click navbar link (scroll) =================    
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active'); 
+    
+    };
+// ================= scroll reveal =================
+    ScrollReveal({
+        // reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay : 200
+    })
+
+    ScrollReveal().reveal('.home-contect, .heading',{origin: 'top'});
+    ScrollReveal().reveal('.home-img, .services-container, .protfolio-box, .contact form' ,{origin: 'bottom'});
+    ScrollReveal().reveal('.home-content h1, .about-img' ,{origin: 'left'});
+    ScrollReveal().reveal('.home-content p, .about-content' ,{origin: 'right'});
+
+// =================  typed js =================
+    const typed = new Typed('.multiple-text',{
+        String: ['Frontend Developer','Graphic Design','DB Designer'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true,
+    });
+
+
+
+
+
+
+
+// ==================Download CV ==================
+document.getElementById("downloadBtn").addEventListener("click", function () {
+    const fileUrl = "asstes/cv/Abdulrahman CV.pdf"; 
+  
+    const link = document.createElement("a");
+    link.href = fileUrl;
+    link.download = "رزومه.pdf"; 
+  
+    document.body.appendChild(link);
+    link.click();
+  
+    document.body.removeChild(link);
+  });
+  
